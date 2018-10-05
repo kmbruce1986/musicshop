b212c25a3a7c65e0ae78caacc2b6ed273ae67872
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-05 10:54:06 +0100

    adds instrument abstract class

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -2,7 +2,25 @@ package instruments;
 
 public abstract class Instrument {
 
+    private String material;
+    private String colour;
+    private Type type;
 
+    public Instrument(String material, String colour, Type type) {
+        this.material = material;
+        this.colour = colour;
+        this.type = type;
+    }
 
+    public String getMaterial() {
+        return material;
+    }
 
+    public String getColour() {
+        return colour;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
